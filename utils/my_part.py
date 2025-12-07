6f68aef71e7eb009dad1b64bfc819554864efb64
BakerMsn <pengzhliang3@mail2.sysu.edu.cn>
2025-12-07 18:17:22 +0800

    首次提交项目代码 -- 20251207 *** 18:11

---
+++ utils/my_part.py
@@ -0,0 +1,4 @@
+
+
+def my_function():
+    print("This is my function from my_part.py")
---
+++ v1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from utils.my_part import my_function
+
+print("NumPy version:", np.__version__)
+
+print("Today is a great day to learn NumPy! -- 2025 12 07")
+
+def greet():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    greet()
+    my_function()
